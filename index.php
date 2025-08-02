8505489ab0455eb8b3abcdc54f7165001dca56a4
MosesR <mosesrinaldi@gmail.com>
2025-08-02 15:37:13 +0700

    menambahkan paragraf baru di index.html

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Coba git remote</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quasi in. Eos animi eligendi, cumque, molestiae laudantium aut nesciunt fuga voluptatum, sapiente velit sunt quae corrupti officiis totam placeat perferendis!</p>
 </body>
 </html>
